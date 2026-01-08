ca8ca1b3d600e136337c3a608d7a869f93791b47
Gridsofvietnamesemodernim <gridsofvietnamesemodernism@gmail.com>
2026-01-08 19:15:24 +0000

    Update index.tsx

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,5 @@
 import { Canvas } from "@react-three/fiber";
-import { Experience } from "./components/Experience";
+import { Experience } from "../components/Experience";
 import styles from "./style.module.css";
 
 /* FIX: We changed 'export default function' to 'export function' 
