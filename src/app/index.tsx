99e7368fe9ef7d6bb0c57ee760ff716bd144ea2a
Gridsofvietnamesemodernim <gridsofvietnamesemodernism@gmail.com>
2026-01-08 21:25:36 +0000

    Update index.tsx

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,20 +1,23 @@
-import { Canvas } from "@react-three/fiber";
-// FIX 1: I changed 'Experience' to 'InfiniteCanvas'
-// FIX 2: I removed the curly braces { } in case it is a default export. 
-// If this fails, we will put the braces back.
-import InfiniteCanvas from "../infinite-canvas"; 
-import styles from "./style.module.css";
+import * as React from "react";
+import manifest from "~/src/artworks/manifest.json";
+import { Frame } from "~/src/frame";
+import { InfiniteCanvas } from "~/src/infinite-canvas";
+import type { MediaItem } from "~/src/infinite-canvas/types";
+import { PageLoader } from "~/src/loader";
 
 export function App() {
+  const [media] = React.useState<MediaItem[]>(manifest);
+  const [textureProgress, setTextureProgress] = React.useState(0);
+
+  if (!media.length) {
+    return <PageLoader progress={0} />;
+  }
+
   return (
     <>
-      <div className={styles.canvas}>
-        <Canvas camera={{ position: [0, 0, 5], fov: 45 }}>
-          <color attach="background" args={["#ececec"]} />
-          {/* FIX 3: Updated the tag name here too */}
-          <InfiniteCanvas />
-        </Canvas>
-      </div>
+      <Frame />
+      <PageLoader progress={textureProgress} />
+      <InfiniteCanvas media={media} onTextureProgress={setTextureProgress} />
     </>
   );
 }
