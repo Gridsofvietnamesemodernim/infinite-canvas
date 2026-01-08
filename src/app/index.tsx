e8ce1d663779e91ffcc16579110843e7f1c51f70
Gridsofvietnamesemodernim <gridsofvietnamesemodernism@gmail.com>
2026-01-08 18:58:05 +0000

    Update index.tsx

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,23 +1,22 @@
-import * as React from "react";
-import manifest from "~/src/artworks/manifest.json";
-import { Frame } from "~/src/frame";
-import { InfiniteCanvas } from "~/src/infinite-canvas";
-import type { MediaItem } from "~/src/infinite-canvas/types";
-import { PageLoader } from "~/src/loader";
+import { Canvas } from "@react-three/fiber";
+import { Leva } from "leva";
+import { Experience } from "./components/Experience";
 
-export function App() {
-  const [media] = React.useState<MediaItem[]>(manifest);
-  const [textureProgress, setTextureProgress] = React.useState(0);
-
-  if (!media.length) {
-    return <PageLoader progress={0} />;
-  }
-
+function App() {
   return (
     <>
-      <Frame />
-      <PageLoader progress={textureProgress} />
-      <InfiniteCanvas media={media} onTextureProgress={setTextureProgress} />
+      <Leva hidden />
+      {/* The 3D Canvas Scene */}
+      <Canvas camera={{ position: [0, 0, 5], fov: 45 }}>
+        <color attach="background" args={["#ececec"]} />
+        <Experience />
+      </Canvas>
+      
+      {/* I have removed the <div className="frame"> section here.
+         Now only the Canvas remains.
+      */}
     </>
   );
 }
+
+export default App;
